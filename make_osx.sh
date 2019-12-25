d6bbe01a2e33bec7507f27792b27d34b5b5f4184
BigOyayubi <55793780+BigOyayubi@users.noreply.github.com>
2019-12-26 01:24:26 +0900

    update osx build shell

--- make_osx.sh
+++ make_osx.sh
@@ -4,6 +4,7 @@ mkdir -p build_osx && cd build_osx
 cmake -GXcode ../
 cd ..
 cmake --build build_osx --config Release
-mkdir -p plugin/Plugins/zstd.bundle/Contents/MacOS/
-cp build_osx/Release/zstd.bundle/Contents/MacOS/zstd plugin/Plugins/zstd.bundle/Contents/MacOS/zstd
-cp build_osx/Release/zstd.bundle/Contents/MacOS/zstd Assets/Plugins/zstd.bundle/Contents/MacOS/zstd
+mkdir -p plugin/Plugins/OSX/zstd.bundle/Contents/MacOS/
+cp build_osx/Release/zstd.bundle/Contents/MacOS/zstd plugin/Plugins/OSX/zstd.bundle/Contents/MacOS/zstd
+mkdir -p Assets/Plugins/OSX/zstd.bundle/Contents/MacOS/
+cp build_osx/Release/zstd.bundle/Contents/MacOS/zstd Assets/Plugins/OSX/zstd.bundle/Contents/MacOS/zstd
